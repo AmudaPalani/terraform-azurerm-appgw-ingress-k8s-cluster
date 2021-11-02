744fc22e66208335b98294da1b613bbf186a0053
Amudha Palani <53053396+AmudaPalani@users.noreply.github.com>
2021-11-02 14:53:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,26 +34,16 @@ resource "azurerm_virtual_network" "test" {
   resource_group_name = data.azurerm_resource_group.rg.name
   address_space       = [var.virtual_network_address_prefix]
 
-  subnet {
-    name           = var.aks_subnet_name
-    address_prefix = var.aks_subnet_address_prefix # Kubernetes Subnet Address prefix
-  }
-
-  subnet {
-    name           = "appgwsubnet" # Has to be hardcoded to this name.
-    address_prefix = var.app_gateway_subnet_address_prefix
-  }
-
   tags = var.tags
 }
 
-data "azurerm_subnet" "kubesubnet" {
+resource "azurerm_subnet" "kubesubnet" {
   name                 = var.aks_subnet_name
   virtual_network_name = azurerm_virtual_network.test.name
   resource_group_name  = data.azurerm_resource_group.rg.name
 }
 
-data "azurerm_subnet" "appgwsubnet" {
+resource "azurerm_subnet" "appgwsubnet" {
   name                 = "appgwsubnet" #Hardcoded to this name.
   virtual_network_name = azurerm_virtual_network.test.name
   resource_group_name  = data.azurerm_resource_group.rg.name
